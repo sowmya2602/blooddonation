ee5762e585683045a9f7674f931dc8e1b14b067e
sowmya2602 <83546307+sowmya2602@users.noreply.github.com>
2021-05-13 22:56:06 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php 
 
 // $con=new mysqli("localhost","root","","blood_bank");
- $con=new mysqli("remotemysql.com","9m7dJyxRoZ",,"","9m7dJyxRoZ");
+ $con=new mysqli("remotemysql.com","4ubnYsQI4i","hueZrhbQ4L","4ubnYsQI4i");
 
 if($con->connect_error)
 {
