8918ecb76f13e17190f65437b7908bba20088b10
sowmya2602 <83546307+sowmya2602@users.noreply.github.com>
2021-05-13 22:42:50 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,8 @@
 <?php 
 
-$con=new mysqli("localhost","root","","blood_bank");
+// $con=new mysqli("localhost","root","","blood_bank");
+ $con=new mysqli("remotemysql.com","9m7dJyxRoZ",,"","9m7dJyxRoZ");
+
 if($con->connect_error)
 {
 	echo "Database Connection Failed";
